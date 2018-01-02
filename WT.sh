78aa6aef6ce0652b9d0bc4848ff1d69f3d8eb247
Evan Lyall <elyall@ln001.brc>
2018-01-02 15:31:10 -0800

    fixed path issue

--- WT.sh
+++ WT.sh
@@ -11,7 +11,7 @@
 #SBATCH --output=/global/home/users/elyall/Logs/WT-%j.out
 #SBATCH --error=/global/home/users/elyall/Errors/WT-%j.err
 source /usr/Modules/init/bash
-export PATH="/global/home/users/elyall/Code/WhiskerTracking/whisk/build:$PATH" # add whisk binaries to path
+export PATH="/global/home/users/elyall/Code/whisk/build:$PATH" # add whisk binaries to path
 cd ~/Code/whisk/ # change to main folder
 
 # SET DIRECTORY TO ANALYZE
@@ -30,7 +30,7 @@ EOF
 
 # CONVERT FILES TO STANDARDIZED FORMATS
 module load python/2.7.8 tables/3.1.1 # load python 2 & tables
-export PYTHONPATH="/global/home/users/elyall/Code/WhiskerTracking/:$PYTHONPATH" # add whisk python modules to path
+export PYTHONPATH="/global/home/users/elyall/Code/:$PYTHONPATH" # add whisk python modules to path
 # python python/batch.py $DIR -e "trace" -f "*.tif"
 python python/whiskers2hdf5.py $DIR
 python python/measurements2mat.py $DIR
